077468cbeeedc46aa3b6c8ddae82def76b220046
Lawrence Caulker <locaulker@gmail.com>
2023-06-03 19:46:04 -0400

    get all and single products implemented

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,18 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from "express"
+import dotenv from "dotenv"
 dotenv.config()
-
-import connectDB from './config/db.js'
-
-import products from './data/products.js'
+import connectDB from "./config/db.js"
+import productRoutes from "./routes/productRoutes.js"
 
 connectDB() // Connects to MongoDB
 
 const port = process.env.PORT
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('API is running...')
+app.get("/", (req, res) => {
+  res.send("API is running...")
 })
 
-app.get('/api/v2/products', (req, res) => {
-  res.json(products)
-})
-
-app.get('/api/v2/products/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id)
-  res.json(product)
-})
+app.use("/api/v2/products", productRoutes)
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
